bddec7d1bcecea296b64d91e50133ca3f879f5ff
Raza <mdahsan.raza@dxc.com>
2020-09-04 20:36:03 +0530

    ready for deployment

--- server/index.js
+++ server/index.js
@@ -78,4 +78,8 @@ app.get('/api/user/logout',auth, (req, res)=>{
 })
 
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log("server running on port ", PORT);
+});
